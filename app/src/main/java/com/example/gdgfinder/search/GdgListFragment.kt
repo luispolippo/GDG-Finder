1a28173a6eb93f7034dcb18f8b9c0d6d3f4309f4
luispolippo <luispolippo@hotmail.com>
2021-05-05 20:12:47 -0300

    added chips for filter in the gdg List

--- app/src/main/java/com/example/gdgfinder/search/GdgListFragment.kt
+++ app/src/main/java/com/example/gdgfinder/search/GdgListFragment.kt
@@ -11,8 +11,10 @@ import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import com.example.gdgfinder.R
 import com.example.gdgfinder.databinding.FragmentGdgListBinding
 import com.google.android.gms.location.*
+import com.google.android.material.chip.Chip
 import com.google.android.material.snackbar.Snackbar
 
 
@@ -54,6 +56,34 @@ class GdgListFragment: Fragment() {
                 }
             }
         })
+
+
+        viewModel.regionList.observe(viewLifecycleOwner, object: Observer<List<String>> {
+            override fun onChanged(data: List<String>?) {
+                data ?: return
+
+                val chipGroup = binding.regionList
+                val inflator = LayoutInflater.from(chipGroup.context)
+
+
+                val children = data.map{ regionName ->
+                    val chip = inflator.inflate(R.layout.region, chipGroup, false) as Chip
+                    chip.text = regionName
+                    chip.tag = regionName
+                    chip.setOnCheckedChangeListener{ button, isChecked ->
+                        viewModel.onFilterChanged(button.tag as String, isChecked)
+                    }
+                    chip
+                }
+
+                chipGroup.removeAllViews()
+
+                for(chip in children){
+                    chipGroup.addView(chip)
+                }
+            }
+        })
+
         setHasOptionsMenu(true)
         return binding.root
     }
